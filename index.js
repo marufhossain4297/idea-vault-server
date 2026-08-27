b110ccbd7ffd7036993cefc171581ae1a30bbd45
Tahmina Haque <tahminahaque@Tahminas-MacBook-Air.local>
2026-08-28 01:38:02 +0200

    comment api created

--- index.js
+++ index.js
@@ -74,6 +74,26 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/comment/:ideaId', async (req, res) => {
+            const id = req.params.ideaId
+            const query = {
+                _id: new ObjectId(id)
+            };
+            const result = await commentsCollection.deleteOne(query)
+            res.send(result)
+        })
+        
+        app.patch('/comment/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedData = req.body;
+
+            const result = await commentsCollection.updateOne(
+                { _id: new ObjectId(id) },
+                { $set: updatedData }
+            );
+            res.send(result);
+        });
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
