c70a77997552d3fbbf140d97fa65549e4843277f
Tahmina Haque <tahminahaque@Tahminas-MacBook-Air.local>
2026-09-03 23:33:58 +0200

    Trying to add search and filter api ut faild

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.listen(port, () => {
     console.log(`Runing in port ${port}`)
 })
 
+
 async function run() {
     try {
         await client.connect();
@@ -32,6 +33,105 @@ async function run() {
         const ideasCollection = db.collection('ideas')
         const commentsCollection = db.collection('comments')
 
+
+        // app.get('/ideas', async (req, res) => {
+
+        //     const AITools = req.query.AITools
+        //     const realEstateTech = req.query.realEstateTech
+        //     const developerTools = req.query.developerTools
+        //     const productivity = req.query.productivity
+        //     const hardwareHealth = req.query.hardwareHealth
+        //     const fintechSaaS = req.query.fintechSaaS
+        //     const noCode = req.query.noCode
+        //     const cleanTech = req.query.cleanTech
+        //     const designTools = req.query.designTools
+        //     const hrTech = req.query.hrTech
+        //     const connectivity = req.query.connectivity
+        //     const healthTech = req.query.healthTech
+        //     const cybersecurity = req.query.cybersecurity
+        //     const eCommerce = req.query.eCommerce
+        //     const eventTech = req.query.eventTech
+        //     const aiMedia = req.query.aiMedia
+
+        //     if (AITools) {
+        //         const result = await ideasCollection.find({ category: "AI Tools" }).toArray()
+        //         res.send(result)
+        //     }
+        //     else if (realEstateTech) {
+        //         const result = await ideasCollection.find({ category: "Real Estate Tech" }).toArray()
+        //         res.send(result)
+        //     }
+        //     else if (developerTools) {
+        //         const result = await ideasCollection.find({ category: "Developer Tools" }).toArray()
+        //         res.send(result)
+        //     }
+        //     else if (productivity) {
+        //         const result = await ideasCollection.find({ category: "Productivity" }).toArray()
+        //         res.send(result)
+        //     }
+        //     else if (hardwareHealth) {
+        //         const result = await ideasCollection.find({ category: "Hardware & Health" }).toArray()
+        //         res.send(result)
+        //     }
+        //     else if (fintechSaaS) {
+        //         const result = await ideasCollection.find({ category: "Fintech & SaaS" }).toArray()
+        //         res.send(result)
+        //     }
+        //     else if (noCode) {
+        //         const result = await ideasCollection.find({ category: "No-Code" }).toArray()
+        //         res.send(result)
+        //     }
+        //     else if (cleanTech) {
+        //         const result = await ideasCollection.find({ category: "CleanTech" }).toArray()
+        //         res.send(result)
+        //     }
+        //     else if (designTools) {
+        //         const result = await ideasCollection.find({ category: "Design Tools" }).toArray()
+        //         res.send(result)
+        //     }
+        //     else if (hrTech) {
+        //         const result = await ideasCollection.find({ category: "HR Tech" }).toArray()
+        //         res.send(result)
+        //     }
+        //     else if (connectivity) {
+        //         const result = await ideasCollection.find({ category: "Connectivity" }).toArray()
+        //         res.send(result)
+        //     }
+        //     else if (healthTech) {
+        //         const result = await ideasCollection.find({ category: "HealthTech" }).toArray()
+        //         res.send(result)
+        //     }
+        //     else if (cybersecurity) {
+        //         const result = await ideasCollection.find({ category: "Cybersecurity" }).toArray()
+        //         res.send(result)
+        //     }
+        //     else if (eCommerce) {
+        //         const result = await ideasCollection.find({ category: "E-commerce" }).toArray()
+        //         res.send(result)
+        //     }
+        //     else if (eventTech) {
+        //         const result = await ideasCollection.find({ category: "EventTech" }).toArray()
+        //         res.send(result)
+        //     }
+        //     else if (aiMedia) {
+        //         const result = await ideasCollection.find({ category: "AI Media" }).toArray()
+        //         res.send(result)
+        //     }
+
+        // })
+
+        // Express Backend
+        
+        
+        app.get('/ideas', async (req, res) => {
+            const category  = req.query.category
+
+            const query = category ? { category: category } : {};
+            const result = await ideasCollection.find(query).toArray();
+
+            res.send(result);
+        });
+        
         app.post('/ideas', async (req, res) => {
             const ideaInfo = req.body
             const result = await ideasCollection.insertOne(ideaInfo)
